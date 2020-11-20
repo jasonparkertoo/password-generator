a477731bdc8ca3180acc317644b642600dd959e1
j.parker <me@jasonparker.info>
2020-11-19 23:54:45 -0500

    clarify test expectations

--- src/test/java/info/jasonparker/password/generator/DefaultGeneratorTest.java
+++ src/test/java/info/jasonparker/password/generator/DefaultGeneratorTest.java
@@ -11,17 +11,17 @@ public class DefaultGeneratorTest {
     private final DefaultGenerator defaultGenerator = new DefaultGenerator(new DefaultRules());
 
     @Test
-    public void getPasswordReturnsExpectedLengthString() {
+    public void givenAPasswordRequest_whenTheRequestIsValid_thenThePasswordWillHaveTheRequestLength() {
         int expected = 12;
         String pw = defaultGenerator.getPassword(expected);
         int actual = pw.length();
-        assertEquals(expected, actual);
+        assertEquals("Should return a 12 character password", expected, actual);
     }
 
     @Test
     public void givenAPasswordRequest_whenTheRequestedLengthIsValid_thenThePasswordMatchesTheRegularExpression() {
         String pw = defaultGenerator.getPassword(20);
         boolean isValid = pw.matches(defaultGenerator.getPattern());
-        assertTrue(isValid);
+        assertTrue("Should match the rule pattern: " + defaultGenerator.getPattern(), isValid);
     }
 }
